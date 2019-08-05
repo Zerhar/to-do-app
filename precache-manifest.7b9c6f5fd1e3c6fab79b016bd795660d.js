5373100b799c76061007185dabcc56f639e47e3a
Zerhar <akkus.robin@gmail.com>
2019-08-05 15:58:10 +0200

    Updates

---
+++ precache-manifest.7b9c6f5fd1e3c6fab79b016bd795660d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7748c87fdb0dc56e65bc42a82dac74e2",
+    "url": "/Zerhar/todo-app/index.html"
+  },
+  {
+    "revision": "9556aa2deff5a421e1c0",
+    "url": "/Zerhar/todo-app/static/css/main.36548ad6.chunk.css"
+  },
+  {
+    "revision": "616d0d115431acfda845",
+    "url": "/Zerhar/todo-app/static/js/2.836f42b8.chunk.js"
+  },
+  {
+    "revision": "9556aa2deff5a421e1c0",
+    "url": "/Zerhar/todo-app/static/js/main.f8815c9e.chunk.js"
+  },
+  {
+    "revision": "e3dd8c0e3dd15e46b70b",
+    "url": "/Zerhar/todo-app/static/js/runtime~main.8863042b.js"
+  }
+]);
